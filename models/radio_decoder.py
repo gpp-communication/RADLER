c77b65c5a8ed65789f512be3a34f682e78f53314
Yuan <chengxuyuan1998@gmail.com>
2024-03-20 22:27:12 +0100

    update radio decoder

--- models/radio_decoder.py
+++ models/radio_decoder.py
@@ -1,14 +1,17 @@
+import torch
 import torch.nn as nn
-class RODDecode(nn.Module):
+
+
+class RODDecoder(nn.Module):
 
     def __init__(self, n_class):
-        super(RODDecode, self).__init__()
-        self.convt1 = nn.ConvTranspose3d(in_channels=256, out_channels=128,
-                                         kernel_size=(4, 6, 6), stride=(2, 2, 2), padding=(1, 2, 2))
-        self.convt2 = nn.ConvTranspose3d(in_channels=128, out_channels=64,
-                                         kernel_size=(4, 6, 6), stride=(2, 2, 2), padding=(1, 2, 2))
-        self.convt3 = nn.ConvTranspose3d(in_channels=64, out_channels=n_class,
-                                         kernel_size=(3, 6, 6), stride=(1, 2, 2), padding=(1, 2, 2))
+        super(RODDecoder, self).__init__()
+        self.convt1 = nn.ConvTranspose2d(in_channels=256, out_channels=128,
+                                         kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
+        self.convt2 = nn.ConvTranspose2d(in_channels=128, out_channels=64,
+                                         kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
+        self.convt3 = nn.ConvTranspose2d(in_channels=64, out_channels=n_class,
+                                         kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
         self.prelu = nn.PReLU()
         self.sigmoid = nn.Sigmoid()
         # self.upsample = nn.Upsample(size=(rodnet_configs['win_size'], radar_configs['ramap_rsize'],
@@ -19,3 +22,11 @@ class RODDecode(nn.Module):
         x = self.prelu(self.convt2(x))  # (B, 128, W/2, 32, 32) -> (B, 64, W, 64, 64)
         x = self.convt3(x)  # (B, 64, W, 64, 64) -> (B, 3, W, 128, 128)
         return x
+
+
+if __name__ == '__main__':
+    rod_decoder = RODDecoder(3)
+    test = torch.randn(1, 256, 16, 16)
+    with torch.no_grad():
+        output = rod_decoder(test)
+        print(output.shape)
