45890fc509b70315bf8c1f8f2b78cb84de27a8fe
Yuan <y.luo@kolb-digital.de>
2024-04-26 14:29:44 +0200

    add pytorch dataset for CRUW

---
+++ data_tools/ssl/CRUW_dataset.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+from CRTUM_dataset import CRTUMDataset
+
+
+class CRUWDataset(CRTUMDataset):
+    def __getitem__(self, idx):
+        img_path = self.df['images'][idx]
+        radar_path = self.df['radar_frames'][idx]
+        image = Image.open(img_path).convert('RGB')
+        radar_frame = np.load(radar_path)
+        if self.image_transform is not None:
+            image = self.image_transform(image)  # TODO: to tensor
+        if self.radar_transform is not None:
+            radar_frame = self.radar_transform(radar_frame)  # TODO: to tensor
+        return image, radar_frame
+
+
+if __name__ == '__main__':
+    CRUW_dataset = CRUWDataset('../../datasets/CRUW')
+    print(CRUW_dataset.__len__())
+
