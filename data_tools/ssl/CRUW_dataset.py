a2b5ff9b85ab31ec615b497a1e2fbb26f88c7d2d
Yuan <y.luo@kolb-digital.de>
2024-04-27 15:12:09 +0200

    fix issue with transform on radar frames

--- data_tools/ssl/CRUW_dataset.py
+++ data_tools/ssl/CRUW_dataset.py
@@ -1,14 +1,21 @@
 import numpy as np
 from PIL import Image
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
 from CRTUM_dataset import CRTUMDataset
 
 
 class CRUWDataset(CRTUMDataset):
     def __getitem__(self, idx):
-        img_path = self.df['images'][idx]
-        radar_path = self.df['radar_frames'][idx]
+        img_path = self.df['image'][idx]
+        radar_path = self.df['radar_frame'][idx]
         image = Image.open(img_path).convert('RGB')
         radar_frame = np.load(radar_path)
+        radar_frame = np.concatenate((radar_frame, np.zeros([128, 128, 1])), axis=-1)
+        # TODO: 128 x 128, which one is the range, and which one is the angle
+        # radar_frame = np.transpose(radar_frame, (2, 0, 1))
+        radar_frame = np.pad(radar_frame, ((48, 48), (48, 48), (0, 0)), 'constant')
         if self.image_transform is not None:
             image = self.image_transform(image)
         if self.radar_transform is not None:
@@ -16,7 +23,31 @@ class CRUWDataset(CRTUMDataset):
         return image, radar_frame
 
 
+def CRUW_dataloader(root, batch_size, num_workers=4, image_transform=None,
+                     radar_frames_transform=None, pin_memory=True):
+    dataset = CRUWDataset(root, image_transform, radar_frames_transform)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory
+    )
+    return dataloader
+
+
 if __name__ == '__main__':
-    CRUW_dataset = CRUWDataset('../../datasets/CRUW')
-    print(CRUW_dataset.__len__())
+    # CRUW_dataset = CRUWDataset('../../datasets/CRUW')
+    img_transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    radar_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    dataloader = CRUW_dataloader('../../datasets/CRUW', batch_size=32, num_workers=4, image_transform=img_transform,
+                                  radar_frames_transform=radar_transform)
+    for i, (images, radar_frames) in enumerate(dataloader):
+        print(images.shape, radar_frames.shape)
 
