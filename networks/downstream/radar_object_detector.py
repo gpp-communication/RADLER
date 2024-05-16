2867f84c4250c2ec5dc991af7d191966bdb8fd73
Yuan Luo <chengxuyuan1998@gmail.com>
2024-05-16 15:24:45 +0200

    test with semantic-depth tensor

--- networks/downstream/radar_object_detector.py
+++ networks/downstream/radar_object_detector.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 from einops.layers.torch import Rearrange
@@ -20,13 +21,15 @@ class RadarObjectDetector(nn.Module):
         self.channel_resize = nn.Conv2d(1280, 256, kernel_size=1, stride=1, padding=0)
         self.norm = nn.BatchNorm2d(256)
 
-    def forward(self, x, semantic_depth_tensor):
+    def forward(self, x, semantic_depth_tensor=None):
         with torch.no_grad():
             x = self.encoder(x)
         x = self.feature_reshape(x)
         x = self.channel_resize(x)
         x = self.norm(x)
         if self.fuse_semantic_depth_feature:
+            assert semantic_depth_tensor is None, \
+                "Semantic depth tensor should not be None when feature fusion is desired"
             semantic_depth_feature = self.semantic_depth_feature_extractor(semantic_depth_tensor)
             x = x + semantic_depth_feature  # Add the semantic depth feature to every channel of the radar frame feature
             x = self.norm(x)
@@ -34,8 +37,11 @@ class RadarObjectDetector(nn.Module):
 
 
 if __name__ == '__main__':
-    model = RadarObjectDetector()
+    model = RadarObjectDetector(fuse_semantic_depth_feature=True)
     test = torch.randn(1, 3, 224, 224)
+    semantic_depth_tensor_test = np.load('../../models/semantic_depth.npy')
+    semantic_depth_tensor_test = np.expand_dims(semantic_depth_tensor_test, 0)
+    semantic_depth_tensor_test = torch.from_numpy(semantic_depth_tensor_test).to(torch.float32)
     model.eval()
     with torch.no_grad():
         output = model(test)
