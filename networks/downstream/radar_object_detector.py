2a3abd50ae666b7be78d90f3126b6d2782b7545a
Yuan Luo <chengxuyuan1998@gmail.com>
2024-05-16 11:36:15 +0200

    add option to fuse semantic depth feature in downstream task

--- networks/downstream/radar_object_detector.py
+++ networks/downstream/radar_object_detector.py
@@ -4,22 +4,32 @@ from einops.layers.torch import Rearrange
 
 from models.radio_decoder import RODDecoder
 from models.ssl_encoder import SSLEncoder
+from models.semantic_depth_feature_extractor import SemanticDepthFeatureExtractor
 
 
 class RadarObjectDetector(nn.Module):
-    def __init__(self, num_class=3):
+    def __init__(self, num_class=3, fuse_semantic_depth_feature=False):
         super(RadarObjectDetector, self).__init__()
+        self.fuse_semantic_depth_feature = fuse_semantic_depth_feature
         self.encoder = SSLEncoder()
         # self.encoder.load_state_dict(torch.load("models/ssl_encoder.pth"))  # TODO: load pretrained weights
         self.decoder = RODDecoder(num_class)
+        if self.fuse_semantic_depth_feature:
+            self.semantic_depth_feature_extractor = SemanticDepthFeatureExtractor()
         self.feature_reshape = Rearrange('b (p1 p2) d -> b d p1 p2', p1=16, p2=16)
         self.channel_resize = nn.Conv2d(1280, 256, kernel_size=1, stride=1, padding=0)
+        self.norm = nn.BatchNorm2d(256)  # TODO: BatchNorm or LayerNorm?
 
-    def forward(self, x):
+    def forward(self, x, semantic_depth_tensor):
         with torch.no_grad():
             x = self.encoder(x)
         x = self.feature_reshape(x)
         x = self.channel_resize(x)
+        x = self.norm(x)
+        if self.fuse_semantic_depth_feature:
+            semantic_depth_feature = self.semantic_depth_feature_extractor(semantic_depth_tensor)  # TODO: BatchNorm?
+            x = x + semantic_depth_feature
+            x = self.norm(x)
         return self.decoder(x)
 
 
