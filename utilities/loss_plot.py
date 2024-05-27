befd6e0711aecac1d68e0dea167b3be2abb1e694
Yuan Luo <chengxuyuan1998@gmail.com>
2024-05-27 22:19:55 +0200

    add flag to indicate whether it is a valid log or not

--- utilities/loss_plot.py
+++ utilities/loss_plot.py
@@ -10,17 +10,20 @@ parser.add_argument('log', type=str)
 def extract_epoch_loss_acc1_acc5(log: str):
     if not log.startswith('Epoch: ['):
         log = log.lstrip('\x00')
-    log = log.replace('      ', '    ')
-    log_items = log.split('	')
-    acc1 = log_items[4].replace('Acc@1', '').lstrip(' ').split(' ')[0]
-    acc1 = float(acc1)
-    acc5 = log_items[5].replace('Acc@5', '').lstrip(' ').split(' ')[0]
-    acc5 = float(acc5)
-    epoch = log_items[0]
-    epoch = epoch.replace('Epoch: [', '')
-    epoch = int(epoch[:epoch.find('][')])
-    loss = float(log_items[3].split(' ')[1])
-    return epoch, loss, acc1, acc5
+    if len(log) == 135 or len(log) == 136:
+        log = log.replace('      ', '    ')
+        log_items = log.split('	')
+        acc1 = log_items[4].replace('Acc@1', '').lstrip(' ').split(' ')[0]
+        acc1 = float(acc1)
+        acc5 = log_items[5].replace('Acc@5', '').lstrip(' ').split(' ')[0]
+        acc5 = float(acc5)
+        epoch = log_items[0]
+        epoch = epoch.replace('Epoch: [', '')
+        epoch = int(epoch[:epoch.find('][')])
+        loss = float(log_items[3].split(' ')[1])
+        return True, epoch, loss, acc1, acc5
+    else:
+        return False, 0, 0, 0, 0
 
 
 def read_train_log(train_log_path):
@@ -35,21 +38,22 @@ def read_train_log(train_log_path):
         acc1_sum = 0
         acc5_sum = 0
         while line := f.readline():
-            epoch, loss, acc1, acc5 = extract_epoch_loss_acc1_acc5(line)
-            count += 1
-            loss_sum += loss
-            acc1_sum += acc1
-            acc5_sum += acc5
-            if epoch != epoch_previous:
-                epochs.append(epoch)
-                loss_values.append(loss_sum / count)
-                acc1_values.append(acc1_sum / count)
-                acc5_values.append(acc5_sum / count)
-                epoch_previous = epoch
-                count = 0
-                loss_sum = 0
-                acc1_sum = 0
-                acc5_sum = 0
+            valid, epoch, loss, acc1, acc5 = extract_epoch_loss_acc1_acc5(line)
+            if valid:
+                count += 1
+                loss_sum += loss
+                acc1_sum += acc1
+                acc5_sum += acc5
+                if epoch != epoch_previous:
+                    epochs.append(epoch)
+                    loss_values.append(loss_sum / count)
+                    acc1_values.append(acc1_sum / count)
+                    acc5_values.append(acc5_sum / count)
+                    epoch_previous = epoch
+                    count = 0
+                    loss_sum = 0
+                    acc1_sum = 0
+                    acc5_sum = 0
     return epochs, loss_values, acc1_values, acc5_values
 
 
