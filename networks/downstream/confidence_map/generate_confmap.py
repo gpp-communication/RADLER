f513af6e50cd4d4568774df979502f7793341640
Yuan Luo <chengxuyuan1998@gmail.com>
2024-06-12 18:14:14 +0200

    fix number of frames

--- networks/downstream/confidence_map/generate_confmap.py
+++ networks/downstream/confidence_map/generate_confmap.py
@@ -167,13 +167,14 @@ if __name__ == '__main__':
     splits = ['train', 'test']
     sites = ['Arcisstrasse1', 'Arcisstrasse2', 'Arcisstrasse3', 'Arcisstrasse4',
              'Arcisstrasse5', 'Gabelsbergerstrasse1', 'Gabelsbergerstrasse2']
-    num_frames = {'train': {'Arcisstrasse1': 1137, 'Arcisstrasse2': 667, 'Arcisstrasse3': 1344, 'Arcisstrasse4': 1313,
-                            'Arcisstrasse5': 1413, 'Gabelsbergerstrasse1': 1075, 'Gabelsbergerstrasse2': 906},
+    num_frames = {'train': {'Arcisstrasse1': 1137, 'Arcisstrasse2': 667, 'Arcisstrasse3': 1344, 'Arcisstrasse4': 1314,
+                            'Arcisstrasse5': 1414, 'Gabelsbergerstrasse1': 1076, 'Gabelsbergerstrasse2': 907},
                   'test': {'Arcisstrasse1': 285, 'Arcisstrasse2': 167, 'Arcisstrasse3': 337, 'Arcisstrasse4': 329,
                             'Arcisstrasse5': 354, 'Gabelsbergerstrasse1': 270, 'Gabelsbergerstrasse2': 227}
                   }
     for split in splits:
         for site in sites:
+            print(split, site)
             anno_path = ("/Users/yluo/Pictures/CRTUM_new/data_cluster_1_2/downstream/" + split + '/' + site +
                          '/Annotations_polar.txt')
             meta_dict = load_anno_txt(anno_path, num_frames[split][site], range_grids, angle_grids)
