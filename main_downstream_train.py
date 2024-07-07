004a8a0dee255375d1763b18105f3692a2d334c4
Yuan Luo <chengxuyuan1998@gmail.com>
2024-07-07 14:02:54 +0200

    use Adam as optimizer

--- main_downstream_train.py
+++ main_downstream_train.py
@@ -249,8 +249,8 @@ def main_worker(gpu, ngpus_per_node, args):
     # define loss function (criterion) and optimizer
     criterion = nn.BCELoss().cuda(args.gpu)
 
-    optimizer = torch.optim.SGD(
-        model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.weight_decay
+    optimizer = torch.optim.Adam(
+        model.parameters(), args.lr, weight_decay=args.weight_decay
     )
 
     # optionally resume from a checkpoint
