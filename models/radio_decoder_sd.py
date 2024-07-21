6937dd9ca307ae57efdcfbe2a9548f98e8dac982
Yuan Luo <chengxuyuan1998@gmail.com>
2024-07-21 20:29:20 +0200

    build separate decoder for fusing semantic depth tensor

--- models/radio_decoder_sd.py
+++ models/radio_decoder_sd.py
@@ -2,36 +2,36 @@ import torch
 import torch.nn as nn
 
 
-class Decoder(nn.Module):
+class DecoderSD(nn.Module):
 
     def __init__(self, n_class):
-        super(Decoder, self).__init__()
-        self.convt1 = nn.ConvTranspose2d(in_channels=256, out_channels=128,
+        super(DecoderSD, self).__init__()
+        self.convt1 = nn.ConvTranspose2d(in_channels=384, out_channels=192,
                                          kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
-        self.convt2 = nn.ConvTranspose2d(in_channels=128, out_channels=64,
+        self.convt2 = nn.ConvTranspose2d(in_channels=192, out_channels=96,
                                          kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
-        self.convt3 = nn.ConvTranspose2d(in_channels=64, out_channels=32,
+        self.convt3 = nn.ConvTranspose2d(in_channels=96, out_channels=48,
                                          kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
-        self.convt4 = nn.ConvTranspose2d(in_channels=32, out_channels=16,
+        self.convt4 = nn.ConvTranspose2d(in_channels=48, out_channels=24,
                                          kernel_size=(6, 6), stride=(2, 2), padding=(2, 2))
-        self.conv = nn.Conv2d(in_channels=16, out_channels=n_class, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels=24, out_channels=n_class, kernel_size=1)
         self.downsample = nn.Upsample(size=(224, 221), mode='bilinear')
         self.prelu = nn.PReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.prelu(self.convt1(x))  # (B, 256, 16, 16) -> (B, 128, 32, 32)
-        x = self.prelu(self.convt2(x))  # (B, 128, 32, 32) -> (B, 64, 64, 64)
-        x = self.prelu(self.convt3(x))  # (B, 64, 64, 64) -> (B, 32, 128, 128)
-        x = self.prelu(self.convt4(x))  # (B, 32, 128, 128) -> (B, 16, 256, 256)
-        x = self.conv(x)                # (B, 16, 256, 256) -> (B, 3, 256, 256)
+        x = self.prelu(self.convt1(x))  # (B, x, 16, 16) -> (B, x, 32, 32)
+        x = self.prelu(self.convt2(x))  # (B, x, 32, 32) -> (B, x, 64, 64)
+        x = self.prelu(self.convt3(x))  # (B, x, 64, 64) -> (B, x, 128, 128)
+        x = self.prelu(self.convt4(x))  # (B, x, 128, 128) -> (B, x, 256, 256)
+        x = self.conv(x)                # (B, x, 256, 256) -> (B, 3, 256, 256)
         x = self.downsample(x)          # (B, 3, 256, 256) -> (B, 3, 224, 221)
         x = self.sigmoid(x)
         return x
 
 
 if __name__ == '__main__':
-    rod_decoder = Decoder(3)
+    rod_decoder = DecoderSD(3)
     test = torch.randn(1, 256, 16, 16)
     with torch.no_grad():
         output = rod_decoder(test)
