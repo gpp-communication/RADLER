afa6347828349ce5c3b137f4cd4eedda33c04d6f
Yuan Luo <chengxuyuan1998@gmail.com>
2024-07-22 10:05:00 +0200

    update detector

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
-from .radio_decoder import RODDecoder
+from .radio_decoder import Decoder
+from .radio_decoder_sd import DecoderSD
 from .ssl_encoder import SSLEncoder
 from .semantic_depth_feature_extractor import SemanticDepthFeatureExtractor
