1cc703266ffee4cebdeba9922091f0e17d79f173
Yuan Luo <chengxuyuan1998@gmail.com>
2024-08-01 11:13:48 +0200

    calculate mse based on OLS

--- networks/downstream/evaluation/evaluate_localization.py
+++ networks/downstream/evaluation/evaluate_localization.py
@@ -2,34 +2,32 @@ import os
 import json
 import numpy as np
 
-from networks.downstream.evaluation.evaluate_ols import read_gt_txt, read_sub_txt
+from networks.downstream.evaluation.evaluate_ols import read_gt_txt, read_sub_txt, compute_ols_dts_gts
 
 
-def calculate_localization_error(sub_dets: dict, gt_dets:dict, num_frames: int):
-    errors_all = [[], [], []]
+def calculate_localization_error(sub_dets: dict, gt_dets:dict, num_frames: int, ols_thres):
+    errors = [[], [], []]
     for i in range(num_frames):
         for c in range(3):  # pedestrian, cyclist, car
-            dets = []
-            gts = []
-            if len(sub_dets[i, c]) > 0 and len(gt_dets[i, c]) > 0:
-                for det in sub_dets[i, c]:
-                    angle = np.rad2deg(det['angle'])
-                    dets.append((det['range'] * np.cos(angle), det['range'] * np.sin(angle)))  # (x, y)
+            olss = compute_ols_dts_gts(gt_dets, sub_dets, i, c)
+            if len(olss):
+                for j in range(olss.shape[0]):
+                    for k in range (olss.shape[1]):
+                        if olss[j][k] > ols_thres:
+                            dt = sub_dets[i, c][j]
+                            gt = gt_dets[i, c][k]
+                            [dt_x, dt_y] = [dt['range'] * np.cos(dt['angle']), dt['range'] * np.sin(dt['angle'])]
+                            [gt_x, gt_y] = [gt['range'] * np.cos(gt['angle']), gt['range'] * np.sin(gt['angle'])]
+                            # Calculate squared differences
+                            squared_diffs = np.power([dt_x - gt_x, dt_y - gt_y], 2)
 
-                for gt in gt_dets[i, c]:
-                    angle = np.rad2deg(gt['angle'])
-                    gts.append((gt['range'] * np.cos(angle), gt['range'] * np.sin(angle)))
-
-                dets.sort(key=lambda x: x[0])
-                gts.sort(key=lambda x: x[0])
-
-                error_sum = 0
-                for j in range(min(len(dets), len(gts))):
-                    mse = np.sqrt(np.sum(np.power([dets[j][0] - gts[j][0], gts[j][1] - gts[j][1]], 2)))
-                    error_sum += mse
-
-                errors_all[c].append(error_sum / min(len(dets), len(gts)))
-    print(errors_all[0])
+                            # Calculate mean squared error
+                            mse = np.sqrt(np.sum(squared_diffs))
+                            errors[c].append(mse)
+    pedestrian_error = np.sum(errors[0]) / len(errors[0]) if len(errors[0]) > 0 else 0
+    cyclist_error = np.sum(errors[1]) / len(errors[1]) if len(errors[1]) > 0 else 0
+    car_error = np.sum(errors[2]) / len(errors[2]) if len(errors[2]) > 0 else 0
+    return pedestrian_error, cyclist_error, car_error
 
 
 def evaluate_localization_error(data_path, submit_dir, truth_dir):
@@ -41,14 +39,23 @@ def evaluate_localization_error(data_path, submit_dir, truth_dir):
     for sub_name, gt_name in zip(sub_names, gt_names):
         if sub_name != gt_name:
             raise AssertionError("wrong submission file names!")
-
+    errors = {
+        'pedestrian': [],
+        'cyclist': [],
+        'car': []
+    }
     for seqid, (sub_name, gt_name) in enumerate(zip(sub_names, gt_names)):
         gt_path = os.path.join(truth_dir, gt_name)
         sub_path = os.path.join(submit_dir, sub_name)
         n_frame = len(os.listdir(os.path.join(data_path, gt_name.rstrip('.txt'), 'IMAGES_0')))
         gt_dets = read_gt_txt(gt_path, n_frame, object_config)
         sub_dets = read_sub_txt(sub_path, n_frame, object_config)
-        calculate_localization_error(sub_dets, gt_dets, n_frame)
+        pedestrian_error, cyclist_error, car_error = calculate_localization_error(sub_dets, gt_dets, n_frame, 0.9)
+        errors['pedestrian'].append(pedestrian_error)
+        errors['cyclist'].append(cyclist_error)
+        errors['car'].append(car_error)
+
+    print(errors)
 
 
 if __name__ == '__main__':
